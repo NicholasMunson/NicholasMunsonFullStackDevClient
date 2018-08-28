a5fd47722ee440f238a7b36f77b9d0090ba307bb
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-08-28 09:41:43 -0600

    add about, contact, and project components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import About from "./About.js"
+import Projects from "./Projects.js"
+import Contact from "./Contact.js"
 import "../styles/navbar.css"
 class Navbar extends Component {
     render() {
@@ -9,9 +12,9 @@ class Navbar extends Component {
                     <Link to="/" className="title, text">Nicholas Munson</Link>
                     <div className="menu"> 
                         <div className="nav-container">
-                            <Link  className="text" to="/About"> About</Link>
-                            <Link  className="text" to="/Projects">Projects</Link>
-                            <Link  className="text" to="/Contact">Contact</Link>
+                            <Link  className="text" to="/About" component={About}> About</Link>
+                            <Link  className="text" to="/Projects" component={Projects}>Projects</Link>
+                            <Link  className="text" to="/Contact" component={Contact}>Contact</Link>
                         </div>
                     </div>
                 </div>
