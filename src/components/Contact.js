62ebab2b36828b4e4b5495ff449573242f718d78
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-08-30 17:28:26 -0600

    add css files for the new componets

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,9 @@
 import React from "react"
-import '../styles/landing.css'
+import '../styles/contact.css'
 
 function Contact(props){
 return(
-    <div className="landing-container">
+    <div className="contact-container">
         <p>Hello from the Contact section</p>
         <p>Hello from the Contact section</p>
         <p>Hello from the Contact section</p>
