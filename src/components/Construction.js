a4cbda7f2244222a96071645b764bc4402468b2a
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-09-26 14:38:08 -0600

    add construction responsive design

--- src/components/Construction.js
+++ src/components/Construction.js
@@ -5,35 +5,36 @@ import "../styles/navbar.css"
 function Construction(props){
         return( 
             <div className="construction-container">
-            <img  id="self" src={require("../images/self-img-no-background.png")} alt="nicholas munson" />
-            <p className="construction-text">Thank you for visiting NTMDev.com I am currently building, modifying, or updating. Please follow the links below to see me work on github, connect on LinkedIn, email me, or download my resume.  </p>
+            <div className="top-imgs">
+                <img  id="self" src={require("../images/self-img-no-background.png")} alt="nicholas munson" />
+                <img  id="speech-bubble" src={require("../images/constructionMessage.png")} alt="speech bubble" />
+            </div>
             <section>
                 <figure>
                     <a href="https://github.com/NicholasMunson" target="_blank">
-                        <img src={require("../images/github.png")} alt="github icon" />
+                        <img className="icon" src={require("../images/github.png")} alt="github icon" />
                     </a>
                     <figcaption className="construction-text">Github</figcaption>
                 </figure>
                 <figure>
                     <a href="https://www.linkedin.com/in/nicholastmunson/" target="_blank">
-                        <img src={require("../images/linkedin.png")} alt="linked in icon" />
+                        <img className="icon"  src={require("../images/linkedin.png")} alt="linked in icon" />
                     </a>
                     <figcaption className="construction-text">LinkedIn</figcaption>
                 </figure>
                 <figure>
                     <a href="mailto:Nicholas.T.Munson@gmail.com"target="_blank">
-                        <img src={require("../images/gmail.png")} alt="G mail icon" />
+                        <img className="icon"  src={require("../images/gmail.png")} alt="G mail icon" />
                     </a>
                     <figcaption className="construction-text">Gmail</figcaption>
                 </figure>
                 <figure>
                     <a href="https://drive.google.com/file/d/1ABBkmt4tpUYFlCQA-kRVm1tTN_yAXAZp/view?usp=sharing" download target="_blank">
-                        <img src={require("../images/resume.png")} alt="Resume Icon" />
+                        <img className="icon"  src={require("../images/resume.png")} alt="Resume Icon" />
                     </a>
                     <figcaption className="construction-text">Resume</figcaption>
                 </figure>
             </section>
-            <p className="construction-text" id="thankyou">Thank you!</p>
             </div>
         )
     }
