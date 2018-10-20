35be16d3a0a3b8d03b7acc92d7f1056aa3ab7733
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-10-19 19:33:22 -0600

    removed unneeded code

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,6 @@ import "../styles/about.css"
 function About(props){
 return(
     <div className="body">
-    <a href="www.google.com">Goolge!</a>
         <figure>
             <img className="hero-image" src={require("../images/imageedit_3_4072812862.jpg")} alt="keyboard" />
         </figure>       
