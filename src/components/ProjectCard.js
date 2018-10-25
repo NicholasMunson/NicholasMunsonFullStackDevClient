dd04d059ebb18e20650a82fd1e9646bc4c12e853
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-10-25 13:15:12 -0600

    use css flex to manage content on cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,6 +4,7 @@ import '../styles/projectCard.css'
 
 const ProjectCard = (props) => {
     let projects = props.projects
+    console.log()
     return (
         <div className="card">
             <div className="img-container">
@@ -11,14 +12,13 @@ const ProjectCard = (props) => {
                 <h1>{projects.name}</h1>
             </div>
             <div className="sections-container">
-                <section className="left-card-section">
-                    
-                    <h3> <a href={projects.linkFrontend}>Check out my frontend code here </a></h3>
-                    <h3> <a href={projects.linkBackend}> Backend code here</a> </h3>
+                <section className="left-card-section">  
+                    <p className="project-description" >{projects.description}</p>    
+                    <p className="project-links"> <a href={projects.linkFrontend}>Check out my frontend code here </a></p> 
                 </section>
-                <section className="right-card-section">
-                    <p>{projects.description}</p>
-                    <p>{projects.tech}</p>
+                <section className="right-card-section"> 
+                    <p className="project-tech">{projects.tech}</p>      
+                    <p className="project-links"> <a href={projects.linkBackend}> Backend code here</a> </p>
                 </section>
             </div>
             
