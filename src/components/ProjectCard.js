f4871e1f886ec59b9c19a78b93ab067bc6bf1bd0
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-10-25 12:48:03 -0600

    style cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,17 +7,20 @@ const ProjectCard = (props) => {
     return (
         <div className="card">
             <div className="img-container">
-                <img src={projects.img} />
-            </div>
-            <section className="left-card-section">
+                <img className="project-img" src={projects.img} alt="" />
                 <h1>{projects.name}</h1>
-                <h3> <a href={projects.linkFrontend}>Check out my frontend code here </a></h3>
-                <h3> <a href={projects.linkBackend}> Backend code here</a> </h3>
-            </section>
-            <section>
-                <p>{projects.description}</p>
-
-            </section>
+            </div>
+            <div className="sections-container">
+                <section className="left-card-section">
+                    
+                    <h3> <a href={projects.linkFrontend}>Check out my frontend code here </a></h3>
+                    <h3> <a href={projects.linkBackend}> Backend code here</a> </h3>
+                </section>
+                <section className="right-card-section">
+                    <p>{projects.description}</p>
+                    <p>{projects.tech}</p>
+                </section>
+            </div>
             
         </div>
     );
