a166287dcc12668c58701c3a937310bd1a982085
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-11-17 09:41:26 -0700

    create hidden menu at view width 425px

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { FaBars, FaClose } from 'react-icons/fa';
+import { FaBars, FaRegWindowClose } from 'react-icons/fa';
 import "../styles/navbar.css"
 import ResponsiveMenu from 'react-responsive-navbar';
 
@@ -7,24 +7,25 @@ class Navbar extends Component {
 
     render() {
         return( 
-            <ResponsiveMenu
-                className="header-container"
-                menuOpenButton={<FaBars size={30} color="MediumPurple" />}
-                menuCloseButton={<FaClose size={30} color="MediumPurple" />}
-                changeMenuOn="500px"
-                largeMenuClassName="large-menu-classname"
-                smallMenuClassName="small-menu-classname"
-                menu={
-                <div className="header-container">
-                    <ul className="nav-container">
-                        <li><a to="/" className="text a-text" href="#landing" >Home</a></li>
-                        <li><a className="text a-text" href="#about-section">Bio</a> </li>
-                        <li><a className="text a-text" href="#projects">Projects</a></li>
-                        <li><a className="text a-text" href="#contact" >Contact</a></li>
-                    </ul>
-                </div>
-                }
-            />
+            <div className="header-container">
+                <ResponsiveMenu
+                    className="menu-container"             
+                    menuOpenButton={<FaBars size={30} color="#BDD990" />}
+                    menuCloseButton={<FaRegWindowClose size={30} color="#BDD990" />}
+                    changeMenuOn="425px"
+                    largeMenuClassName="large-menu-classname"
+                    smallMenuClassName="small-menu-classname"
+                    menu={
+                
+                        <ul className="nav-container">
+                            <li><a to="/" className="text a-text" href="#landing" >Home</a></li>
+                            <li><a className="text a-text" href="#about-section">Bio</a> </li>
+                            <li><a className="text a-text" href="#projects">Projects</a></li>
+                            <li><a className="text a-text" href="#contact" >Contact</a></li>
+                        </ul>
+                    }
+                />
+            </div>
                 // <div className="header-container"> 
                 //     <a to="/" className="title" ><img id="nm-logo" src={require("../images/ntmlogo1000.png")} alt="Nicholas Munson logo"/></a>
                 //     <div className="menu"> 
