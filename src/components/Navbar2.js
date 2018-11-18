4e51d9826d1a9a67a89c0e99ab99b3b1a2d40db8
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-11-18 13:04:12 -0700

    Create new nav component

---
+++ src/components/Navbar2.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react"
+import { FaBars, FaRegWindowClose } from 'react-icons/fa';
+import "../styles/navbar.css"
+import ResponsiveMenu from 'react-responsive-navbar';
+
+
+class Navbar extends Component {
+
+    render() {
+        return( 
+        <div>
+            <ResponsiveMenu
+                menuOpenButton={<FaBars size={30} color="#BDD990" />}
+                menuCloseButton={<FaRegWindowClose size={30} color="#BDD990" />}
+                changeMenuOn="500px"
+                largeMenuClassName="large-menu"
+                smallMenuClassName="small-menu"
+                menu={
+                    <ul className="nav-container">
+                            <li>
+                                <a className="text a-text" href="#landing" >Home</a>
+                            </li>
+                            <li>
+                                <a className="text a-text" href="#about-section">Bio</a> 
+                            </li>
+                            <li>
+                                <a className="text a-text" href="#projects">Projects</a>
+                            </li>
+                            <li>
+                                <a className="text a-text" href="#contact" >Contact</a>
+                            </li>
+                        </ul>
+                }
+            />  
+        </div>
+        )
+    }
+} 
+
+
+export default Navbar
