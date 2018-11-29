bc34f1d1f83c3947d3316e3313c2060e6831e286
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-11-28 21:58:22 -0700

    added margin to bottom of contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,13 +10,13 @@ function Contact(props){
             </div>
             <div className="icon-cluster media-container">
                 <section>
-                    <figure>
+                    <figure className="contact-icon">
                         <a href="https://github.com/NicholasMunson"  rel="noopener noreferrer"  target="_blank">
                             <img className="icon" src={require("../images/github.png")} alt="github icon" />
                         </a>
                         <figcaption className="construction-text">Github</figcaption>
                     </figure>
-                    <figure>
+                    <figure className="contact-icon">
                         <a href="https://www.linkedin.com/in/nicholastmunson/"  rel="noopener noreferrer"  target="_blank">
                             <img className="icon"  src={require("../images/linkedin.png")} alt="linked in icon" />
                         </a>
@@ -24,13 +24,13 @@ function Contact(props){
                     </figure>
                 </section>
                 <section>    
-                    <figure>
+                    <figure className="contact-icon">
                         <a href="mailto:Nicholas.T.Munson@gmail.com"  rel="noopener noreferrer"  target="_blank">
                             <img className="icon"  src={require("../images/gmail.png")} alt="G mail icon" />
                         </a>
                         <figcaption className="construction-text">Gmail</figcaption>
                     </figure>
-                    <figure>
+                    <figure className="contact-icon">
                         <a href="https://drive.google.com/file/d/1gdkTz1ADv9teX5VF0_yRqNi4xIlSgyjQ/view?usp=sharing" download  rel="noopener noreferrer"  target="_blank">
                             <img className="icon"  src={require("../images/resume.png")} alt="Resume Icon" />
                         </a>
