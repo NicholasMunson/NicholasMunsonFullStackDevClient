776bb8db8214fc80e4ac69fd99528058cf08ae8c
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-12-10 11:50:00 -0700

    re-added controls

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ function Landing(props){
 return(
     <div className="body" id="landing">
         <div className="media-container">
-            <video className="background-and-border" loop autoPlay >
+            <video className="background-and-border" loop autoPlay controls >
                 <source src={require("../videos/ntmdev.com_intro_video.mp4")} type="video/mp4" />
             </video>
         </div>
