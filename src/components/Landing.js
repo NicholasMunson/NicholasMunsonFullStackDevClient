5baa3c6139bb7cdcc43975f93130e8c53b33521f
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-12-10 11:39:48 -0700

    video refrance to local drive

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,14 @@
 import React from "react"
 import '../styles/landing.css'
-const URL = "https://vimeo.com/303836781"
+// const URL = "https://drive.google.com/file/d/1WwgHqF1rgtct-0xUBfOfqADXCokAgKgz/view?usp=sharing"
+// const intro = "../videos/ntmdev.com_intro_video.mp4"
 
 function Landing(props){
 return(
     <div className="body" id="landing">
         <div className="media-container">
             <video className="background-and-border" loop autoPlay controls>
-                <source src={URL}/>
+                <source src={require("../videos/ntmdev.com_intro_video.mp4")} type="video/mp4" />
             </video>
         </div>
     </div>
