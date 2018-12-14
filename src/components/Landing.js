819b78e333bbbcf81e507eabb379d085e737dcd8
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-12-13 19:26:00 -0700

    changed the landing video

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,9 +6,9 @@ function Landing(props){
 return(
     <div className="body" id="landing">
             <div className="video">
-                <iframe title="ntmdev video" src="https://streamable.com/s/kf5y2/upctn?autoplay=1" frameBorder="0" width="100%" height="100%" allowFullScreen >                  
-                </iframe>
+                <iframe title="ntm_welcome_video" src="https://streamable.com/s/kf5y2/izacel" frameBorder="0" width="100%" height="100%" allowFullScreen />
             </div>
+            
     </div>
 
 )
