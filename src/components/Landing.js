bdcfe975c1e77604e42c12f7fa19437af2dc70e2
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-12-14 09:53:54 -0700

    changed video src

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,9 +6,8 @@ function Landing(props){
 return(
     <div className="body" id="landing">
             <div className="video">
-                <iframe title="ntm_welcome_video" src="https://streamable.com/s/kf5y2/izacel" frameBorder="0" width="100%" height="100%" allowFullScreen />
-            </div>
-            
+                <iframe title="ntm_welcome_video" src="https://player.vimeo.com/video/303836781?autoplay=1&loop=1&title=0&byline=0&portrait=0" frameBorder="0" width="100%" height="100%" allowFullScreen autoPlay />
+            </div>          
     </div>
 
 )
